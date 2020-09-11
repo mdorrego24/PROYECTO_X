123dedb6ebb49255e5e68f3e525a4c926e999a78
mdorrego24 <68927451+mdorrego24@users.noreply.github.com>
2020-09-11 20:40:08 -0300

    Add files via upload

--- js/product-info.js
+++ js/product-info.js
@@ -1,8 +1,7 @@
-//Funcion para mostras informacion del producto seleccionado en este caso es el mismo para cualquier producto
-
 var productInfo = {};
+var prodRel = {};
 
-function showImagesGallery(array){
+function showImagesGallery(array){//funcion para mostrar las imagenes en forma de galeria
 
     let htmlContentToAppend = "";
 
@@ -20,12 +19,7 @@ function showImagesGallery(array){
         document.getElementById("productInfoImagesGallery").innerHTML = htmlContentToAppend;
     }
 }
-
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e){//funcion que ejecuta la extraccion de informacion del json y se muestra en el html
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
@@ -33,34 +27,91 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             let productInfoNameHTML  = document.getElementById("Name");
             let productInfoDescriptionHTML = document.getElementById("Description");
-            let productInfoCostHTML = document.getElementById("Cost");
-            let productInfoCurrencyHTML = document.getElementById("Currency");
+            let productInfoCurrencyCostHTML = document.getElementById("CurrencyCost");
             let productInfoSoldCountHTML = document.getElementById("soldCount");
             let productInfoCategoryHTML = document.getElementById("Category");
-            let productInfoRelatedProductsHTML = document.getElementById("relatedProducts");
 
             productInfoNameHTML.innerHTML = productInfo.name;
             productInfoDescriptionHTML.innerHTML = productInfo.description;
-            productInfoCostHTML.innerHTML = productInfo.cost;
-            productInfoCurrencyHTML.innerHTML = productInfo.currency;
+            productInfoCurrencyCostHTML.innerHTML = productInfo.currency + " " + productInfo.cost;
             productInfoSoldCountHTML.innerHTML = productInfo.soldCount;
             productInfoCategoryHTML.innerHTML = productInfo.category;
-            productInfoRelatedProductsHTML.innerHTML= productInfo.relatedProducts;
 
-            //Muestro las imagenes en forma de galería
+
             showImagesGallery(productInfo.images);
+
+                        
+
 
             
 
+            }
+    });
+    function showRelatedProducts(array){// funcion para mostrar los productos relacionados
+
+    
+        let htmlContentToAppend = "";
+    
+        for (let i = 0; i < array.length; i++) {
+            var relIndex = array[i];
+            var relProd = prodRel[relIndex];
+        
+            htmlContentToAppend += `
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="d-block mb-4 h-100">
+                    <img class="img-fluid img-thumbnail" src="` + relProd.imgSrc + `" alt="">
+                <h4 class="mb-1">`+ relProd.name +`</h4>
+                <p class="mb-1">` + relProd.description + `</p>
+                <a href="product-info.html">Ver producto</a>
+                </div>
+            </div>
+            `
         }
+            document.getElementById("relatedProducts").innerHTML = htmlContentToAppend;
+    }
+
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok") 
+        
+        {
+             prodRel = resultObj.data; }
+
+        showRelatedProducts(productInfo.relatedProducts);
     });
 });
-    
-    function enviar(){
-    var comentario=document.getElementById("comment").value;
-    document.getElementById("comentarios").innerHTML='<p> '+ comentario +' </p>';  
+
+
+
+function showStars(stars){// funcion para mostrar la puntuacion de los usuarios sacados del json en forma de estrellas
+
+    let estrellas  = "";
+
+    for(let i = 0; i < stars; i++){
+        estrellas += `<span style="font-size: 30px; color:orange;">★</span>`
+    }
+    for(let i = stars; i < 5; i++){
+        estrellas += `<span style="font-size: 30px; color: grey;">★</span>`
+    }
+    return estrellas
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){//funcion de extraccion de comentarios 
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+           
+           var infoComments = resultObj.data;
+           console.log(infoComments);
+
+           for(let comentario of infoComments) {
+               let comentariosDOM = document.getElementById("comentariosDOM");
+               comentariosDOM.innerHTML += `
+                <p><b>${comentario.user}</b>${" " + comentario.dateTime + " "} ${showStars(parseInt(comentario.score))}</p>
+                <p>${comentario.description}</p><br><hr><br>
+               `;
+           }
+           
+        }
+    });
 });
